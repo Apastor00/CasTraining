e79a1f7d579825074272055e9e1933b2ba7b483c
logan <artemipastor@gmail.com>
2022-05-29 15:09:16 +0200

    V 1.1 - Commit prueba

--- app/src/main/java/com/castraining/castraining/Prueba.java
+++ app/src/main/java/com/castraining/castraining/Prueba.java
@@ -2,10 +2,13 @@ package com.castraining.castraining;
 
 public class Prueba {
     private String prueba;
+    private String prueba2;
 
-    public Prueba(String prueba) {
+    public Prueba(String prueba, String prueba2) {
         this.prueba = prueba;
+        this.prueba2 = prueba2;
     }
+
     public Prueba (){}
 
     public String getPrueba() {
@@ -14,5 +17,13 @@ public class Prueba {
 
     public void setPrueba(String prueba) {
         this.prueba = prueba;
+    }
+
+    public String getPrueba2() {
+        return prueba2;
+    }
+
+    public void setPrueba2(String prueba2) {
+        this.prueba2 = prueba2;
     }
 }
