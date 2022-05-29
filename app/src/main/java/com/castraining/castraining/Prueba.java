dfe278d5c4cdbcf254842fc3494721cde958858a
logan <artemipastor@gmail.com>
2022-05-29 13:00:37 +0200

    V 1.1 - Commit prueba

---
+++ app/src/main/java/com/castraining/castraining/Prueba.java
@@ -0,0 +1,18 @@
+package com.castraining.castraining;
+
+public class Prueba {
+    private String prueba;
+
+    public Prueba(String prueba) {
+        this.prueba = prueba;
+    }
+    public Prueba (){}
+
+    public String getPrueba() {
+        return prueba;
+    }
+
+    public void setPrueba(String prueba) {
+        this.prueba = prueba;
+    }
+}
